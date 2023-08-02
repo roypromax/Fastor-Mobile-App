631b3eb2db1b1b821adb57f20c0dfb796600719a
roypromax <roy19rishabh@gmail.com>
2023-08-03 04:46:17 +0530

    Update OTP component

--- Frontend/src/components/Otp.jsx
+++ Frontend/src/components/Otp.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Otp.module.css";
+
+const otpArray = ["", "", "", "", "", ""];
 
 const Otp = () => {
-  return <div>Otp</div>;
+  const [otp, setOtp] = useState(otpArray);
+  const navigate = useNavigate();
+  const otpRefs = otpArray.map((element, index) => {
+    return useRef(null);
+  });
+
+  const handleChange = (index, value) => {
+    value = value.slice(0, 1);
+
+    setOtp((prev) => {
+      let newOtp = [...prev];
+      newOtp[index] = value;
+      return newOtp;
+    });
+
+    if (value !== "") {
+      otpRefs[index + 1].current.focus();
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (otp.join("") === "123456") {
+      alert("OTP verified");
+      navigate("/dashboard");
+    } else {
+      alert("Invalid OTP");
+      setOtp(otpArray);
+      otpRefs[0].current.focus();
+    }
+  };
+
+  return (
+    <div className={styles.otpContainer}>
+      <h1 className={styles.title}>OTP Verification</h1>
+      <p className={styles.subtitle}>
+        Enter the verification code we just sent on your Mobile Number.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.inputContainer}>
+          {otp.map((value, index) => (
+            <input
+              type="number"
+              value={value}
+              onChange={(e) => handleChange(index, e.target.value)}
+              key={index}
+              ref={otpRefs[index]}
+              required
+              className={styles.inputField}
+            />
+          ))}
+        </div>
+        <button type="submit" className={styles.verifyButton}>
+          Verify
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Otp;
