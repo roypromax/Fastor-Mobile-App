7fff9354e689581595ff8c6977ed2f123c1cf149
roypromax <roy19rishabh@gmail.com>
2023-08-03 05:18:50 +0530

    Set up Dashboard route and component

--- Frontend/src/AllRoutes.jsx
+++ Frontend/src/AllRoutes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Otp from "./components/Otp";
+import Dashboard from "./components/Dashboard";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/otp" element={<Otp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 };
